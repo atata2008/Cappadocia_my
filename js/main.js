513f7b25b0314cb88ee8bf34946c1b2274ce0ea3
Tatyana Averkova <averkova.tatyana@gmail.com>
2020-06-16 00:28:56 +0300

    Did first slider and partially titles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+    $('.fairy-tail__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-back.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
 });
